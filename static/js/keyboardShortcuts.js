ad710d8e18131a6828a60a48996900635a280917
Nannan <nanna7077@gmail.com>
2022-01-24 12:58:15 +0530

    Remove: Prevent Defaults for keyboard shortcuts

--- static/js/keyboardShortcuts.js
+++ static/js/keyboardShortcuts.js
@@ -1,21 +1,17 @@
 window.addEventListener('keyup', function(e) {
     if (e.shiftKey && e.key === 'O') {
         openFile()
-        e.preventDefault();
     }
     if (e.shiftKey && e.key === 'N') {
         newFile()
     }
     if (e.shiftKey && e.key === 'S') {
         saveFile()
-        e.preventDefault();
     }
     if (e.shiftKey && e.key === 'W') {
         closeTab(currentTab)
-        e.preventDefault();
     }
     if (e.key === 'F2') {
         renameFile()
-        e.preventDefault();
     }
 }, false);
